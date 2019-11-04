a57355535a55778fba7e34fec98d27ed33531957
Taylor <taylor@pasq.net>
2019-11-03 23:00:55 -0700

    update insert method

--- controllers/campaigns.controller.js
+++ controllers/campaigns.controller.js
@@ -1,6 +1,8 @@
 
 const db = require('../lib/Postgres').db();
 const config = require('config');
+const crypto = require('crypto')
+
 
 
 exports.newCampaign = async function (req, res) {
@@ -9,10 +11,22 @@ exports.newCampaign = async function (req, res) {
     
     try {
         console.log('in TRY');
+        let hash = crypto.createHash('md5');
+
+        if (req.body.recipient_email){
+            hash = hash.update('taylor@pasq.net').digest("hex");
+        }
+        
         await db('campaigns')
-        .insert({ structure: req.body.fields[0]})
+        .insert({
+            name: req.body.campaign_name, 
+            recipient_email_hashed: hash, 
+            recipient_email:  req.body.recipient_email,
+            form_type: req.body.form_type,
+            fields:  req.body.fields[0]
+        })
     } catch (err) {
-        console.error(error.message);
+        console.error(err.message);
         res.status(500).send('Server error');
 
     }

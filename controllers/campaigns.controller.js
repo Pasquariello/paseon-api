60aca35ab70ab1385840dcfcdf7285d25e5af446
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro.local>
2019-11-04 13:05:15 -0700

    rework db schema

--- controllers/campaigns.controller.js
+++ controllers/campaigns.controller.js
@@ -16,19 +16,34 @@ exports.newCampaign = async function (req, res) {
         if (req.body.recipient_email){
             hash = hash.update('taylor@pasq.net').digest("hex");
         }
-        
-        await db('campaigns')
-        .insert({
-            name: req.body.campaign_name, 
-            recipient_email_hashed: hash, 
-            recipient_email:  req.body.recipient_email,
-            form_type: req.body.form_type,
-            fields:  req.body.fields[0] // TODO: fix this so it stores fields unique to forms, need to create a config file to make data structre for build it forms ? 
-        }).returning('*')
-        .then((item) => {
-            res.json(item);     // respond back to request
 
-        });      
+       
+
+            await db('campaigns').insert({
+                 campaign_name: req.body.campaign_name, 
+                //  user_id: 1  //todo this will need to be based on logged in user
+                })
+            .returning('id')
+            .then( response => {
+                console.log('response.id', response[0])
+                const fieldsToInsert = req.body.fields.map(field => 
+                    ({  
+                        campaign_id: response[0],
+                        tag: field.tag, 
+                        type: field.type,
+                        label: field.label,
+                        name: field.name,
+                        value: field.value,
+                        options: field.options,
+                        campaign_name: req.body.campaign_name,
+                        user_id: 1,
+                    })); 
+        
+              return db('campaign_forms').insert(fieldsToInsert)
+                  .then(() => { console.log('Succeess')})
+                  .catch((err) => {console.log('Error', err)});
+            });
+    
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');

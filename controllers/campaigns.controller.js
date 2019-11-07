1f8df46e64962c6267604433fd17ff8ecad862dd
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro.local>
2019-11-07 00:06:01 -0700

    update query to handle new DB schema

--- controllers/campaigns.controller.js
+++ controllers/campaigns.controller.js
@@ -25,185 +25,21 @@ exports.campaignDetails = async function (req, res){
     console.log('hit details!')
     console.log(req.params)
     try {
-
-        // await db('campaign_responses').select('id', 'campaign_id', 'field', 'submission_id').groupBy('campaign_responses.submission_id').then(resp =>{
-        //     console.log('resp', resp)
-        // })
-
-        // .leftJoin('accounts', function() {
-        //     this.on('accounts.id', '=', 'users.account_id').orOn('accounts.owner_id', '=', 'users.id')
-        //   })
-
-        //  SELECT films.id, films.title, films.genre, count(*) as number_of_reviews FROM films LEFT JOIN reviews ON films.id = reviews.film_id GROUP BY films.id;
-        // db.select('campaigns.id').leftJoin('campaign_responses', function(){
-        //     this.on('campaign_responses.campaign_id', '=', 'campaigns.id')
-        //     }).groupBy('campaign_forms.submission_id').then(d=>{
-        //         console.log('hi', d)
-        // })
-
-     
-        // db.from('campaigns')
-        // .innerJoin('campaign_responses', 'campaigns.id', 'campaign_responses.campaign_id')
-        // .then(d => {
-        //     console.log('======', d)
-        // })
-
-        // db.select('submission_id', 'field').from('campaign_responses').groupBy('submission_id', 'field')
-        // .then(d => {
-        //     console.log('======', d)
-        // })
-    
-        ////////////////////////////////////////////////
-        class Resp extends Model {
-
-            // Table name is the only required property.
-            static get tableName() {
-              return 'campaign_responses';
-            }
-          
-            // Each model must have a column (or a set of columns) that uniquely
-            // identifies the rows. The column(s) can be specified using the `idColumn`
-            // property. `idColumn` returns `id` by default and doesn't need to be
-            // specified unless the model's primary key is something else.
-            static get idColumn() {
-              return 'id';
-            }
-          
-            // Methods can be defined for model classes just as you would for
-            // any JavaScript class. If you want to include the result of these
-            // method in the output json, see `virtualAttributes`.
-            // fullName() {
-            //   return this.firstName + ' ' + this.lastName;
-            // }
-          
-            // Optional JSON schema. This is not the database schema!
-            // No tables or columns are generated based on this. This is only
-            // used for input validation. Whenever a model instance is created
-            // either explicitly or implicitly it is checked against this schema.
-            // See http://json-schema.org/ for more info.
-            static get jsonSchema () {
-              return {
-                type: 'object',
-                //required: ['firstName', 'lastName'],
-          
-                properties: {
-                  id: {type: 'integer'},
-                  submission_id: {type: 'integer'},
-                //   firstName: {type: 'string', minLength: 1, maxLength: 255},
-                //   lastName: {type: 'string', minLength: 1, maxLength: 255},
-                //   age: {type: 'number'},
-          
-                  // Properties defined as objects or arrays are
-                  // automatically converted to JSON strings when
-                  // writing to database and back to objects and arrays
-                  // when reading from database. To override this
-                  // behaviour, you can override the
-                  // Model.jsonAttributes property.
-                //   address: {
-                //     type: 'object',
-                //     properties: {
-                //       street: {type: 'string'},
-                //       city: {type: 'string'},
-                //       zipCode: {type: 'string'}
-                //     }
-                //   }
-                }
-              };
-            }
-        }
-            ////////////////////////////////////////////////
-
-            // const resp = await Resp.query(db)
-            // console.log('REEEE', resp )
-   
-           
-
-        // db('orders')
-        // .select(
-        //   'orders.*', 
-        // )
-        // .leftJoin(
-        //   knex('charts')
-        //     .select('id_order', knex.raw('count(*) as ??', ['unread']))
-        //     .where('read_by_use', 0).groupBy('id_order').as('x'), 
-        //   'x.id_order', 
-        //   'orders.id_order'
-        // )
-        // .where('id_customer', 42)
-        // .orderBy('date_submitted')
-
-
-        // await db('campaigns').select('fields').where({
-        //     id: req.params.id
-        // }).then(data_schema => {
-        //     db('campaign_forms').where({            
-        //            campaign_id: req.params.id
-        //         }).then(form_data => {
-        //             console.log('TAYLOR', data_schema, form_data)
-        //             res.json({
-        //                 data_schema,
-        //                 form_data
-        //             });
-        //         })
-        // })
-
-
-
-// await db('campaign_responses').distinct(db.raw('ON (submission_id) submission_id, field'))
-//   .orderBy('submission_id').then(d => {
-//       console.log('SYLS', d)
-//   })
-
-// await db
-//     .select([
-//         // Select all fields from table a
-//         'campaign_responses.*',
-//         // Select only id field from table b
-//         'campaign_forms.id as b_id'
-//     ])
-//     .from('table_a')
-//     .leftJoin(
-//         'campaign_responses.*',
-//         'campaign_forms',
-//         'campaign_forms.id'
-//     ).then(t => {
-//         console.log('==========', t)
-//     });
-// CURRENT WORKING VERSION
-        // await db('campaigns').select('fields').where({
-        //     id: req.params.id
-        // }).then(data_schema => {
-        //     db('campaign_responses').select('field_values').where({            
-        //            campaign_id: req.params.id
-        //         }).then(form_data => {
-        //         //    console.log('TAYLOR', form_data)
-        //           db('campaign_responses').distinct('submission_id').then( data =>{
-        //             res.json({
-        //                 data_schema,
-        //                 form_data,
-        //                 data
-        //             });
-        //           })
-               
-        //         })
-        // })
-        await db('campaigns').select('fields').where({
+    // CURRENT WORKING VERSION
+        await db('campaigns').select('schema').where({
             id: req.params.id
         }).then(data_schema => {
             db('campaign_responses').select('*').where({            
                    campaign_id: req.params.id
                 }).then(form_data => {
                 //    console.log('TAYLOR', form_data)
-                  db('campaign_responses').distinct('submission_id').then( data =>{
                     res.json({
                         data_schema,
                         form_data,
-                        data
                     });
-                  })
                
-                })
-        })
+                });
+        });
 
 
 
@@ -223,11 +59,11 @@ exports.newCampaign = async function (req, res) {
         if (req.body.recipient_email){
             hash = hash.update('taylor@pasq.net').digest("hex");
         }
-
+   
             await db('campaigns').insert({
                 campaign_name: req.body.campaign_name, 
                 user_id: 2,  //todo this will need to be based on logged in user
-                fields: req.body.fields,
+                schema: req.body.fields,
 
             })
             .returning('id')

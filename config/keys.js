824872d8be6f99fa1c3854ef66684708a4e50b8c
Taylor <taylor@pasq.net>
2019-12-11 01:34:39 -0700

    fix postgres spelling

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //make object available outside of this file
 module.exports = {
-    postgesURI: 'postgres://wauttbpxrvztfz:5c37479438cffc6f7c0c824b11c862a04bd91dfc64615ccaae62aa942fd17c49@ec2-54-235-96-48.compute-1.amazonaws.com:5432/dbrl4u2q3crloc?ssl=true',
+    postgresURI: 'postgres://wauttbpxrvztfz:5c37479438cffc6f7c0c824b11c862a04bd91dfc64615ccaae62aa942fd17c49@ec2-54-235-96-48.compute-1.amazonaws.com:5432/dbrl4u2q3crloc?ssl=true',
     // secretOrKey: 'n!gh+H3r0n'
     STRIPE_SECRET_KEY: "sk_test_LE4KxozC6O8d3krb3FEjbBp00erufO2Bm",
     STRIPE_PUBLISHABLE_KEY: "pk_test_KT4KxozC6O8d3krb3FEjbBp00eruf93Bh"
